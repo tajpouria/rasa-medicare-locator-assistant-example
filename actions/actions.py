70614ad37e71f47ad79b469ae58ecd0607109706
tajpouria <pouria.tajdivand@betconstruct.com>
2022-02-22 16:39:52 +0400

    feat: wip form

--- actions/actions.py
+++ actions/actions.py
@@ -6,7 +6,7 @@
 
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
